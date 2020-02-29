d0f69466f6127fff100b85574b1fa6129c8fd79b
Vyacheslav Nagel <v9gel@mail.ru>
2020-02-29 08:17:52 +0700

    * немного верстки на странице входа

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,8 @@
 </template>
 
 <style>
+* {
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-    <h3>Login</h3>
-    <button @click="login">Войти</button>
+  <div class="flex-wrapper">
+    <el-card class="box-card">
+      <div class="content">
+        <h1>deepverification</h1>
+        <p>
+          Приветственный текст сверху, в двух словах про то какие проблемы
+          решает проект и все такое на несколько строк.
+        </p>
+        <p>
+          Да это украдено с медиум.
+        </p>
+        <el-button @click="login" class="button">
+          <img class="google" src="../assets/google.svg" />
+          Продолжить с Google</el-button
+        >
+        <p id="bottom-text">Еще немного текста снизу, может со ссылками или копирайтами.</p>
+      </div>
+    </el-card>
   </div>
 </template>
 
@@ -25,3 +40,46 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.flex-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.box-card {
+  width: 50%;
+  min-width: 500px;
+  height: 60vh;
+  margin-bottom: 10vh;
+  display: flex;
+  /*align-items: center;*/
+  justify-content: center;
+  text-align: center;
+}
+
+.content {
+  margin: 0 200px 0 200px;
+  height: 100%;
+  position:relative;
+}
+
+.google {
+  vertical-align: text-bottom;
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+}
+
+.button {
+  margin: 20px;
+}
+
+#bottom-text {
+  position: absolute;
+  bottom: 0px;
+}
+</style>
