6f2c508837cd9409c0345034bb66fcb94c156e8a
Vyacheslav Nagel <v9gel@mail.ru>
2020-03-09 02:14:43 +0700

    + страница списка проектов с таблицей

---
+++ src/components/Menu.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">deepverification</el-menu-item>
+      <el-submenu index="2" style="float: right;">
+        <template slot="title">
+          <el-avatar :size="32" :src="user.photoURL"></el-avatar>
+          {{ user.displayName }}
+        </template>
+        <el-menu-item index="2-3" @click="logout">Выйти</el-menu-item>
+      </el-submenu>
+      <el-menu-item
+        index="3"
+        style="float: right;"
+        @click="dialogFormVisible = true"
+        >Новый проект</el-menu-item
+      >
+    </el-menu>
+
+    <el-dialog title="Новый проект" :visible.sync="dialogFormVisible">
+      <el-form :model="form" :rules="rules" ref="form">
+        <el-form-item prop="name" label="Название проекта">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="cancelCreate('form')">Отмена</el-button>
+        <el-button type="primary" @click="createProject('form')"
+          >Создать</el-button
+        >
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import firebase, { db } from "../firebase";
+
+export default {
+  name: "Menu",
+  data: () => ({
+    dialogFormVisible: false,
+    form: {
+      name: ""
+    },
+    rules: {
+      name: [
+        {
+          required: true,
+          message: "Пожалуйста, введите название проекта",
+          trigger: "blur"
+        },
+        {
+          min: 3,
+          max: 50,
+          message: "Длинна должна быть от 3 до 50 символов",
+          trigger: "blur"
+        }
+      ]
+    }
+  }),
+  props: ["user"],
+  methods: {
+    logout() {
+      firebase.auth().signOut();
+    },
+    createProject(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          db.collection("projects").add({
+            name: this.form.name,
+            author: { displayName: this.user.displayName, uid: this.user.uid },
+            lastEdit: Date.now()
+          });
+          this.form.name = "";
+          this.dialogFormVisible = false;
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    cancelCreate(formName) {
+      this.$refs[formName].resetFields();
+      this.dialogFormVisible = false;
+    }
+  }
+};
+</script>
+
+<style scoped></style>
